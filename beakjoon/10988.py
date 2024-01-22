f0019af17bc6e0943860504ef136f66aacd6526d
hom237 <jinsic4568@gmail.com>
2024-01-23 00:32:07 +0900

    what
    when i solve it?

--- beakjoon/10988.py
+++ beakjoon/10988.py
@@ -4,3 +4,9 @@ for i in range(0, len(word)//2):
     if word[i]!=word[len(word)-i-1]:
         result = 0
 print(result)
+# a = input()
+# re_a = a[::-1]
+# if re_a == a:
+#     print(1)
+# else:
+#     print(0)
